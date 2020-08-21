e5bfdaa8b6ef81472a5c84b23a6dd67007d410fb
Hleb <gsviderskiy@bk.ru>
2020-08-21 12:18:03 +0300

    ff

--- js/script.js
+++ js/script.js
@@ -1,29 +1,23 @@
 
-"use strict";
 
-console.log(4 + +'5');
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели ?');
 
-let incr = 10,
-    decr = 10;
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: {},
+    private: false
+};
 
-//++incr;
-//decr--;
 
-console.log(incr++);
-console.log(--decr);
+const a = prompt('Один из последних просмотренных фильмов ?', ''),
+      b = prompt('На сколько оцените его ?', '' ),
+      c = prompt('Один из последних просмотренных фильмов ?', ''),
+      d = prompt('На сколько оцените его ?', '');
 
-console.log(5%2);
 
-console.log(2+2*2 !==6);
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies[c] = d;
 
-&&
-
-||
-
-const isChecked = false,
-      isClose = false;
-
-      console.log(isChecked || !isClose);
-
-    
-
+      console.log(personalMovieDB);
